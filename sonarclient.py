3c932245857aa7aea41bd76e53b459987202db3c
dissi <moreiras@informatik.uni-freiburg.de>
2019-11-16 02:29:36 +0100

    rename sonar-client now sonarclient

--- sonarclient.py
+++ sonarclient.py
@@ -3,13 +3,13 @@ import asyncio
 import re
 import json
 
+
 class SonarClient:
 
     def __init__(self, base_url, island):
-      self.base_url = base_url
-      self.island = island
-      self.session = aiohttp.ClientSession()
-      
+        self.base_url = base_url
+        self.island = island
+        self.session = aiohttp.ClientSession()
 
     async def info(self):
         return await self._request({
@@ -22,19 +22,19 @@ class SonarClient:
             'method': 'PUT',
             'path': ['_create', name],
             'data': {}
-            })
+        })
 
     async def get_schema(self, schemaName):
         schemaName = schemaName.re('/', '-')
         return await self._request({
             'path': [self.island, 'schema', schemaName]
-            })
+        })
 
     async def put_schema(self, schemaName, schema):
         return await self._request({
-        'method': 'PUT',
-         'path': [self.island, 'schema', schemaName],
-         'data': schema
+            'method': 'PUT',
+            'path': [self.island, 'schema', schemaName],
+            'data': schema
         })
 
     async def put(self, record):
@@ -44,27 +44,30 @@ class SonarClient:
         if (id):
             method = 'PUT'
             path.append(id)
-        return await self._request({ "path":path, 'method':method, 'data': value })
-
+        return await self._request({"path": path, 'method': method, 'data': value})
 
     async def _request(self, opts):
         url = opts.get('url')
         if url is None:
-            url = self.base_url +'/'+ opts.get('path')[0]+'/'+ opts.get('path')[1]
+            url = self.base_url + '/' + \
+                opts.get('path')[0]+'/' + opts.get('path')[1]
         aio_opts = {
-        'method': opts['method'] or 'GET',
-        'url': url,
-        'headers': {'Content-Type': 'application/json'},
-        'data': opts['data'] or {},
-        'responseType': opts.get('responseType') }
+            'method': opts['method'] or 'GET',
+            'url': url,
+            'headers': {'Content-Type': 'application/json'},
+            'data': opts['data'] or {},
+            'responseType': opts.get('responseType')}
         if aio_opts.get('method').lower() == 'put':
             async with self.session.put(url) as resp:
-                return await resp.text()  
-             
+                return await resp.text()
+
+
 async def main():
+    print("short example: create a island and get a key:")
     client = SonarClient('http://localhost:9191/api', 'default')
     resp = await client.create_island('telegram')
     key = json.loads(resp).get('key')
     print(key)
+    await client.put('{Hello World}')
     await client.session.close()
 asyncio.run(main())
