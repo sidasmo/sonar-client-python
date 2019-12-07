10be98eafe5c5f6884db3e6d9a2af17473090ed8
mororor <matthias@syndikat.org>
2019-12-07 22:50:02 +0100

    implementation of expand_schema as "/"-bug fix

--- sonarclient/sonarclient.py
+++ sonarclient/sonarclient.py
@@ -1,7 +1,6 @@
 import aiohttp
 import json
 
-
 class SonarClient:
 
     def __init__(self, base_url, island):
@@ -18,25 +17,27 @@ class SonarClient:
     async def create_island(self, name):
         return await self._request({
             'method': 'PUT',
-            'path': ['_create', name],
+            'ath': ['_create', name],
             'data': {}
         })
 
     async def get_schema(self, schemaName):
-        schemaName = schemaName.replace('/', '-')
+        schemaName = expand_schema(schemaName)
         return await self._request({
             'path': [self.island, 'schema', schemaName]
         })
 
     async def put_schema(self, schemaName, schema):
+        schemaName = expand_schema(schemaName)
         return await self._request({
             'method': 'PUT',
             'path': [self.island, 'schema', schemaName],
-            'data': json.dumps(schema)
+            'data': schema
         })
 
     async def put(self, record):
         schema = record.get('schema')
+        schema = expand_schema(schema)
         value = record.get('value')
         id = record.get('id')
         path = [self.island, 'db', schema]
@@ -64,33 +65,10 @@ class SonarClient:
             json=opts.get('data') or {},
         ) as resp:
             return await resp.text()
-        # if aio_opts.get('method').lower() == 'put':
-        #     async with self.session.put(url, json=data as resp:
-        #         return await resp.text()
-        # elif aio_opts.get('method').lower() == 'get':
-        #     async with self.session.get(url) as resp:
-        #         return await resp.text()
-        # elif aio_opts.get('method').lower() == 'post':
-        #     async with self.session.post(url, json=data) as resp:
-        #         return await resp.text()
 
 
-# async def main():
-#     print("short example: create a island and get a key:")
-#     client = SonarClient('http://localhost:9191/api', 'default')
-#     resp = await client.create_island('example_island')
-#     key = json.loads(resp).get('key')
-#     print(key)
-#     from exampleschema import get_doc, get_telegram_media_schema
-#     schema = get_telegram_media_schema()
-#     resp = await client.put_schema("example_schema", schema)
-#     schema = json.loads(resp)
-#     schema_name = schema.get('schema')
-#     print(schema_name)
-#     record_id = None
-#     print("put record to sonar with example_schema, get record_id: ")
-#     record = schema_name, record_id, get_doc()
-#     record_id = json.loads(await client.put(record))
-#     print(record_id.get('id'))
-#     await client.session.close()
-# asyncio.run(main())
+def expand_schema (schema):
+    if "/" in schema:
+        return schema
+    else:
+        return "_/" + schema
