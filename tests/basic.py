a87ae1767e33ef0221b59e0ee7857b3264277732
osuiowq <moreiras@informatik.uni-freiburg.de>
2020-05-12 15:56:27 +0200

    add Possibility to specify another endpoint and another island

--- tests/basic.py
+++ tests/basic.py
@@ -1,19 +1,24 @@
 import unittest
 from sonarclient import SonarClient
-import aiohttp
+import sys
 import asyncio
 
 
 class TestClientMethods(unittest.TestCase):
+    ENDPOINT = 'http://localhost:9191/api'
+    ISLAND = 'default'
 
     def test_put_get_schemas(self):
-        client = SonarClient(endpoint='http://localhost:9191/api',island='default')
+        client = SonarClient(self.ENDPOINT, self.ISLAND)
         loop = asyncio.get_event_loop()
         schemas = loop.run_until_complete(client.get_schemas())
         loop.run_until_complete(client.close())
         loop.close()
         assert "core/schema" in schemas
-        print(schemas)
+
 
 if __name__ == '__main__':
+    if len(sys.argv) > 1:
+        TestClientMethods.ENDPOINT = sys.argv.pop()
+        TestClientMethods.ISLAND = sys.argv.pop()
     unittest.main()
