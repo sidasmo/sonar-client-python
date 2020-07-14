a9498a61716d2af23d2ec18a26f43787123a5707
osuiowq <moreiras@informatik.uni-freiburg.de>
2020-07-14 21:21:53 +0200

    add conftest

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,17 +1,2 @@
-import sys
-
-import py
-import pytest
-from xprocess import ProcessStarter
-
-def start_sonar_server(path,xprocess):
-    class Starter(ProcessStarter):
-        pattern = "listening on http://localhost:9191"
-        args = ['node', path + '/sonar-server/launch.js', '--dev', '-s' '/tmp']   
-    xprocess.ensure("sonarServer", Starter)
-    
-def stop_server(xprocess):
-    xprocess.getinfo("sonarServer").terminate()  
-
 def pytest_addoption(parser):
     parser.addoption("--path", action="store", help="USAGE: pytest tests/basic.py --path [/path/to/sonar]")
